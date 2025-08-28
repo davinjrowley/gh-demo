17ce2e241ed8285946bde73b08f44059abc155a4
davinjrowley <davin.j.rowley@gmail.com>
2025-08-28 14:30:37 -0500

    It now shouts at you

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 if __name__ == "__main__":
-    print("Hello!")
+    s = input("Tell me something to shout back: ")
+    s = s.upper()
+    print("I WILL NOW SHOUT THAT BACK AT YOU!!!!")
+    print(s)
+
